e5cdf73bd1f1a2498802ccef969784a814f8e5ac
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-09 09:41:09 +1200

    add routes for 3 new pages

--- routes.py
+++ routes.py
@@ -9,5 +9,20 @@ def home():
     return render_template("home.html", title="HOME")
 
 
+@app.route('/allhardware')
+def all_hardware():
+    return render_template("all_hardware.html", title="all hardware")
+
+
+@app.route('/hardware')
+def hardware():
+    return render_template("hardware.html", title="hardware")
+
+
+@app.route('/software')
+def software():
+    return render_template("software.html", title="software")
+
+
 if __name__ == "__main__":
     app.run(debug=True)

94d93e56ab0389a950a3ccde19f2034d64d812aa
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-06-10 10:12:04 +1200

    Made route for all_hardware.html

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 
 
 app = Flask(__name__)
@@ -11,7 +12,13 @@ def home():
 
 @app.route('/allhardware')
 def all_hardware():
-    return render_template("all_hardware.html", title="all hardware")
+    conn = sqlite3.connect('HARDWARE.db')
+    cur = conn.cursor()
+    cur.execute()
+    hardwares = cur.fetchall()
+    conn.close()
+    hardware = hardwares[id-1]
+    return render_template("all_hardware.html", title="all_hardware", hardware=hardware, hardwares=hardwares)
 
 
 @app.route('/hardware>')

29bc0103b398b4750d8d0f2c23ef1c4abd76c34e
22197 <22197@burnside.school.nz>
2025-06-23 09:40:32 +1200

    comment code, write explanation about website,

--- routes.py
+++ routes.py
@@ -6,36 +6,32 @@ app = Flask(__name__)
 
 #route for home
 @app.route('/')
-#define function home()
 def home():
     return render_template("home.html", title="HOME")
 
 #route for all hardware
 @app.route('/allhardware')
-#define function all_hardware()
 def all_hardware():
-    conn = sqlite3.connect('HARDWARE.db') #connect database
+    conn = sqlite3.connect('HARDWARE.db')
     cur = conn.cursor()
-    cur.execute('SELECT hw_id, hw_name FROM Hardware;') #Query
+    cur.execute('SELECT hw_id, hw_name FROM Hardware;') 
     hardwares = cur.fetchall()
     conn.close()
     return render_template("all_hardware.html", title="all_hardware", hardwares=hardwares)
 
 #route for specific hardware
 @app.route('/hardware/<int:id>')
-#define function hardware()
 def hardware(id):
     conn = sqlite3.connect('HARDWARE.db')
     cur = conn.cursor()
     cur.execute('SELECT * FROM Hardware;')
-    hardwares = cur.fetchall()
-    hardware = hardwares[id-1]
+    hardwares = cur.fetchall() #fetch all hardware, creating a list of tuples
+    hardware = hardwares[id-1] #since id starts from 1, subtract 1 to get the correct index
     conn.close()
     return render_template("hardware.html", title="hardware", id=id, hardware=hardware)
 
 #route for specific hardware
 @app.route('/software')
-#define function software()
 def software():
     return render_template("software.html", title="software")
 

96e5c014e652c31b0ec8fb25aab28971ad367ec7
22197 <22197@burnside.school.nz>
2025-06-23 09:15:55 +1200

    start commenting code in routes.py

--- routes.py
+++ routes.py
@@ -4,23 +4,26 @@ import sqlite3
 
 app = Flask(__name__)
 
-
+#route for home
 @app.route('/')
+#define function home()
 def home():
     return render_template("home.html", title="HOME")
 
-
+#route for all hardware
 @app.route('/allhardware')
+#define function all_hardware()
 def all_hardware():
-    conn = sqlite3.connect('HARDWARE.db')
+    conn = sqlite3.connect('HARDWARE.db') #connect database
     cur = conn.cursor()
-    cur.execute('SELECT hw_id, hw_name FROM Hardware;')
+    cur.execute('SELECT hw_id, hw_name FROM Hardware;') #Query
     hardwares = cur.fetchall()
     conn.close()
     return render_template("all_hardware.html", title="all_hardware", hardwares=hardwares)
 
-
+#route for specific hardware
 @app.route('/hardware/<int:id>')
+#define function hardware()
 def hardware(id):
     conn = sqlite3.connect('HARDWARE.db')
     cur = conn.cursor()
@@ -30,8 +33,9 @@ def hardware(id):
     conn.close()
     return render_template("hardware.html", title="hardware", id=id, hardware=hardware)
 
-
+#route for specific hardware
 @app.route('/software')
+#define function software()
 def software():
     return render_template("software.html", title="software")
 

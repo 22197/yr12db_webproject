3437b812878fb9033e94e4a1d080495c936093fd
22197 <22197@burnside.school.nz>
2025-08-29 09:40:29 +1200

    fix feedback for hardware.html and commenting

--- routes.py
+++ routes.py
@@ -6,13 +6,15 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 # route for home
 @app.route('/')
 def home():
     return render_template("home.html", title="HOME")
 
+
 # route for all hardware
-@app.route('/allhardware', methods = ['GET'])
+@app.route('/allhardware', methods=['GET'])
 def all_hardware():
     conn = sqlite3.connect('HARDWARE.db')
     cur = conn.cursor()
@@ -23,12 +25,12 @@ def all_hardware():
                            hardwares=hardwares)
 
 
-#route for specific hardware
+# route for specific hardware
 @app.route('/hardware/<int:id>')
 def hardware(id):
     conn = sqlite3.connect('HARDWARE.db')
     cur = conn.cursor()
-    #Query --> get all the items in table Hardware connected to table MediaType and ConsoleType
+    # Query --> get all the items in table Hardware, MediaType and ConsoleType
     cur.execute('''SELECT
                     h.hw_id,
                     h.hw_name,
@@ -47,41 +49,43 @@ def hardware(id):
                 
                 WHERE h.hw_id = ?
                 ''', (id,))
-    hardware = cur.fetchone() #finding one row in the table Hardware with the id that was passed in the url
-    #Query --> get all softwareseries that are related to Hardware
+    hardware = cur.fetchone()  # finding one row in the table Hardware with the id that was passed in the url
+    # Query --> get all softwareseries that are related to Hardware
     cur.execute('''SELECT sw_id, sw_name FROM SoftwareSeries WHERE sw_id IN (
                  SELECT sw_id FROM HardSoft WHERE hw_id = (
                  SELECT hw_id FROM Hardware WHERE hw_id = ?));''', (id,))
-    softwareseries = cur.fetchall()#creating a list of tuples "softwareseries"
+    softwareseries = cur.fetchall()  # creating a list of tuples
     conn.close()
-    return render_template("hardware.html", title="hardware", id=id, hardware=hardware, softwareseries=softwareseries)
+    return render_template("hardware.html", title="hardware", id=id,
+                            hardware=hardware, softwareseries=softwareseries)
 
-#route for specific hardware
+
+# route for specific hardware
 @app.route('/softwareseries/<int:id>')
 def software(id):
-
     conn = sqlite3.connect('HARDWARE.db')
     cur = conn.cursor()
     cur.execute('''SELECT * FROM SoftwareSeries WHERE sw_id = ?''', (id,))
     software_series = cur.fetchone()
     conn.close()
-    return render_template("software.html", title="software", software_series=software_series)
-    
+    return render_template("software.html", title="software",
+                           software_series=software_series)
 
-#route for search bar
-@app.route('/search_result', methods = ['GET'])  
-def search(): #initaly copy student teacher's code --> adapt and improve it
+
+# route for search bar
+@app.route('/search_result', methods=['GET'])  
+def search():  # initaly copy student teacher's code --> adapt and improve it
     search_query = request.args.get('query', '')
-    search_type = request.args.get('type', 'name')  # 'name' or 'series' => help from Github Copilot
+    search_type = request.args.get('type', 'name')  # 'name' or 'series'
     conn = sqlite3.connect('HARDWARE.db')
     if search_query:
         if search_type == 'series':
-            #Query --> get all hardware that is related to SoftwareSeries from search_query
+            # Query --> get all hardware that is related to SoftwareSeries from search_query
             cursor = conn.execute('''SELECT hw_id, hw_name, hw_image FROM Hardware WHERE hw_id IN (
                  SELECT hw_id FROM HardSoft WHERE sw_id IN (
                  SELECT sw_id FROM SoftwareSeries WHERE sw_name LIKE ?));''', ('%' + search_query + '%',))
         elif search_type == 'name':
-            #Query --> get all hardware that is related to Hardware from search_query
+            # Query --> get all hardware related from search_query
             cursor = conn.execute('''SELECT hw_id, hw_name, hw_image FROM Hardware WHERE hw_name LIKE ?''', ('%' + search_query + '%',))    
     else:
         cursor = conn.execute('SELECT hw_id, hw_name, hw_image FROM Hardware')
@@ -89,12 +93,14 @@ def search(): #initaly copy student teacher's code --> adapt and improve it
     conn.close()
     return render_template('search_result.html', hardwares=hardwares, search_query=search_query)
 
-#attributons page route
+
+# attributons page route
 @app.route('/attributions')
 def attributions():
     return render_template("attributions.html", title="attributions")
 
-#route for 404 error
+
+# route for 404 error
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404 
